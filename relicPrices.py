f89e9ade5e9311c1e7c42f16f5be939fade066ea
Akash Brar <owner@Owners-MacBook-Pro-4.local>
2025-02-17 12:12:56 -0700

    Minor fixes and added documentation

--- relicPrices.py
+++ relicPrices.py
@@ -2,9 +2,15 @@ import requests
 import time
 import json
 
+# Relics.json from warframe-items (https://github.com/WFCD/warframe-items)
+# Warframe Market API (https://warframe.market/api_docs)
+
 WARFRAME_MARKET_URL = "https://api.warframe.market/v1"
 
 
+# Calculates timmed/truncated mean, to remove extreme outliers
+# Input: List(int)
+# Output: 0 or float
 def trimmedMean(prices, proportionToCut=0.2):
     n = len(prices)
     k = int(n * proportionToCut)
@@ -16,7 +22,9 @@ def trimmedMean(prices, proportionToCut=0.2):
     else:
         return sum(trimmedPrices) / len(trimmedPrices)
 
-
+# Gets the prices from each order, and seperates them into buy and sell prices
+# Input: json (orders of a single item)
+# Output: 0 or float
 def getPrices(orders):
     buyPrices = []
     sellPrices = []
@@ -28,6 +36,9 @@ def getPrices(orders):
     return trimmedMean(buyPrices), trimmedMean(sellPrices)
 
 
+# Fetches the item orders from Warframe Market
+# Input: string
+# Output: json (orders of a single item)
 def getItemOrders(urlName):
     url = f"{WARFRAME_MARKET_URL}/items/{urlName}/orders"
     headers = {"Accept": "application/json", "User-Agent": "YourAppName/1.0"}
@@ -39,14 +50,18 @@ def getItemOrders(urlName):
         return f"Error: {response.status_code}"
 
 
-def fetchOrders(intactRelics):
+# Gets the buy and sell value of each item in each relic, and adds them to the
+# parsed relic json
+# Input: json (parsed/intact relics)
+# Output: json (parsed/intact relics with prices)
+def getValues(intactRelics):
     total = len(intactRelics)
     relicCount = 0
     fetchCount = 0
 
     for relic in intactRelics:
         relicCount += 1
-        print(f"{relicCount}/{total}")
+        print(f"{relicCount}/{total}  {(relicCount/total)*100}%")
 
         for item in relic["items"]:
             if "Forma" in item["name"]:
@@ -67,7 +82,12 @@ def fetchOrders(intactRelics):
 
     return intactRelics
 
-
+# Parses Relics.json to get all unique relics, with their name, warframe
+# market url, vaulted status, and the items in each relic. It also parses each
+# item in each relic to get their namee, warframe market url, rarity, and
+# chance.
+# Input: string
+# Output: json (parsed relics)
 def getIntactRelics(filename):
     with open(filename, "r", encoding="utf-8") as file:
         relics = json.load(file)
@@ -105,12 +125,17 @@ def getIntactRelics(filename):
 def main():
     relicFile = "Relics.json"
     intactRelics = getIntactRelics(relicFile)
-    relicValues = fetchOrders(intactRelics)
+    relicValues = getValues(intactRelics)
 
     with open("RelicValues.json", "w", encoding="utf-8") as file:
         json.dump(relicValues, file, indent=2)
 
 
 if __name__ == "__main__":
-    # items = ["ward_recovery", "undercroft_dax_camp_scene", "undercroft_lodging_scene", "undercroft_lunaro_scene", ]
     main()
+
+# Example url names with low orders:
+# "ward_recovery"
+# "undercroft_dax_camp_scene"
+# "undercroft_lodging_scene"
+# "undercroft_lunaro_scene"
